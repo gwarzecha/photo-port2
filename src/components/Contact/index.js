044aa3ddd8141436abd8eaaae1ac67adea896f26
gwarz <gmwarzecha@tutanota.com>
2021-04-29 16:44:57 -0700

    added validation to the contact form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,42 +1,69 @@
 import React, { useState } from 'react';
+import { validateEmail } from '../../assets/utils/helpers'
 
 function ContactForm() {
   const [formState, setFormState] = useState({ name: '', email: '', message: '' });
 
   const { name, email, message } = formState;
+  const [errorMessage, setErrorMessage] = useState('');
 
-  function handleChange(e) {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!errorMessage) {
+      setFormState({ [e.target.name]: e.target.value });
+      console.log('Form', formState);
+    }
+  };
+
+  const handleChange = (e) => {
+    if (e.target.name === 'email') {
+      const isValid = validateEmail(e.target.value);
+      if (!isValid) {
+        setErrorMessage('Your email is invalid.');
+      } else {
+        setErrorMessage('');
+      }
+    } else {
+      if (!e.target.value.length) {
+        setErrorMessage(`${e.target.name} is required.`);
+      } else {
+        setErrorMessage('');
+      }
+    }
     // uses setFormState to updated the formState value for the name property;
     // assigns the value taken from the input field with e.target.value, assigns
     //this value to the property e.target.name. The [] create dynamic property names.
     //The spread operator is used to 
     //retain the other key-value pairs in the obj; without ... , the formState
     //obj would be overwritten to only contain name: value
-    setFormState({ ...formState, [e.target.name]: e.target.value })
-  }
+    if (!errorMessage) {
+      setFormState({ ...formState, [e.target.name]: e.target.value });
+    }
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    console.log(formState);
   }
 
   return (
     <section>
-      <h1>Contact me</h1>
-      <form id='contact-form' onSubmit={handleSubmit}>
+      <h1 data-testid="h1tag">Contact me</h1>
+      <form id="contact-form" onSubmit={handleSubmit}>
         <div>
           <label htmlFor="name">Name:</label>
-          <input type="text" defaultValue={name} onChange={handleChange} name="name" />
+          <input type="text" name="name" defaultValue={name} onBlur={handleChange} />
         </div>
         <div>
           <label htmlFor="email">Email address:</label>
-          <input type="email" defaultValue={email} onChange={handleChange} name="email" />
+          <input type="email" name="email" defaultValue={email} onBlur={handleChange} />
         </div>
         <div>
           <label htmlFor="message">Message:</label>
-          <textarea name="message" defaultValue={message} onChange={handleChange} rows="5" />
+          <textarea name="message" rows="5" defaultValue={message} onBlur={handleChange} />
         </div>
-        <button type='submit'>Submit</button>
+        {errorMessage && (
+          <div>
+            <p className="error-text">{errorMessage}</p>
+          </div>
+        )}
+        <button data-testid="button" type="submit">Submit</button>
       </form>
     </section>
   );
