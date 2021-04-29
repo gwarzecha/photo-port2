574ddd2566d7097fcb366f9629f07c72e64ad459
gwarz <gmwarzecha@tutanota.com>
2021-04-28 17:16:01 -0700

    added markup structure for the contact form

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -3,6 +3,8 @@ import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Nav from '..';
 
+
+
 // configure the testing environment
 afterEach(cleanup);
 // declare what the test suite will be testing:
@@ -33,7 +35,9 @@ describe('emoji is visible', () => {
 describe('links are visible', () => {
   it('inserts text into the links', () => {
     const { getByTestId } = render(<Nav />);
+
     expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
     expect(getByTestId('about')).toHaveTextContent('About me');
   });
+
 })
